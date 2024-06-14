bfa0d2e1ceab3d02f6cb76c9590fbba14e4924a8
antonii1357 <antonii.spam@gmail.com>
2024-06-13 20:47:03 -0600

    Added navbar, might change CSS in the future

---
+++ frontend/src/components/Nav/nav.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './nav.css'
+
+export default function Nav() {
+    return (
+        <nav>
+            <ul>
+                <li><a href="#">Libro Random</a></li>
+                <li><a href="#">Subir un Libro</a></li>
+            </ul>
+        </nav>
+    )
+}
