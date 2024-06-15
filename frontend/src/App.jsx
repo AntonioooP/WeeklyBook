39946d36903008e4cc77456dcf02d7ef0efedc7b
antonii1357 <antonii.spam@gmail.com>
2024-06-14 20:24:28 -0600

    Added frontend fetch for getting backend books, needs optimization in the future

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,17 +1,47 @@
-import { useState } from 'react'
+import {useState, useEffect} from 'react'
 import Card from './components/Card/card'
 import Nav from './components/Nav/nav'
 import './App.css'
 
 export default function App() {
-    return (
-        <>
-            <Nav></Nav>
-            <div className="content">
-                <Card title={"Test"} description="test test" date="13-06-2024" image={"https://avatars.githubusercontent.com/u/75284544?v=4"}></Card>
-                <Card title={"Test"} description="test test" date="13-06-2024" image={"https://avatars.githubusercontent.com/u/75284544?v=4"}></Card>
-                <Card title={"Test"} description="test test" date="13-06-2024" image={"https://avatars.githubusercontent.com/u/75284544?v=4"}></Card>
-            </div>
-        </>
-    )
+	const [data, setData] = useState([])
+	const [images, setImages] = useState({})
+	useEffect(() => {
+		const fetchData = async () => {
+			try {
+				const response = await fetch('http://localhost:4200/books')
+				const ids = await response.json()
+
+				const dataPromises = ids.map(async (id) => {
+					const dataResponse = await fetch(`http://localhost:4200/bookData/${id}`)
+					const data = await dataResponse.json()
+
+					const imageResponse = await fetch(`http://localhost:4200/image/${id}`)
+					const imageBlob = await imageResponse.blob()
+					const imageUrl = URL.createObjectURL(imageBlob)
+
+					return {id, data, imageUrl}
+				})
+
+				const allData = await Promise.all(dataPromises)
+
+				const data = allData.map((item) => item.data)
+				const images = Object.fromEntries(allData.map((item) => [item.id, item.imageUrl]))
+
+				setData(data)
+				setImages(images)
+			} catch (error) {
+				console.error('Error fetching data or images:', error)
+			}
+		}
+
+		fetchData()
+	}, [])
+	return (
+		<>
+			<Nav></Nav>
+            <div className='content'>{ data.length ? data.map((item, index) => <Card key={ index } title={ item.title } description={ item.description } date={ item.date } image={ images[ item.id ] }></Card>) :
+            <h1>Cargando...</h1> }</div>
+		</>
+	)
 }
